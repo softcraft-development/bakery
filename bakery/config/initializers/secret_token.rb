040812803b75f6f591fd63c1ab114683f276c87d
Craig Walker <github@softcraft.ca>
2010-05-22 12:37:24 -0600

    Basic Rails install

---
+++ bakery/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '899e6bb5a4096c73c20d4475acac2bd595538a0cc7397e91a1a31963f28959a461e198b6a96b1470f54eecdb7e594685e49d9ee9f1d75c52f599005dfdd61d1a'
