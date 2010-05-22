30f6ec1b3963d5050674723767b646384d9bac86
Craig Walker <github@softcraft.ca>
2010-05-22 13:37:05 -0600

    Disabled shoulda due to rails 3 bug

--- bakery/test/functional/home_controller_test.rb
+++ bakery/test/functional/home_controller_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+  # TODO: Reenable for Shoulda on Rails 3
+  # context "The Home" do
+  #   context "show page" do
+  #     setup do
+  #       get :show
+  #     end
+  #     
+  #     should_respond_with :success
+  #     should_render_template :show
+  #     should_not_set_the_flash      
+  #   end    
+  # end
 end
