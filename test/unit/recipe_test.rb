2a1fbcd1f41fb68531c3c7051695b5543c65b517
Craig Walker <github@softcraft.ca>
2010-05-23 11:06:04 -0600

    Added test_add_ingredients

--- test/unit/recipe_test.rb
+++ test/unit/recipe_test.rb
@@ -9,4 +9,12 @@ class RecipeTest < ActiveSupport::TestCase
     recipe = Factory.build(:recipe, :name => nil)
     assert !Recipe.new.valid?
   end
+
+  def test_add_ingredients
+    recipe = Factory.build(:recipe)
+    recipe.save!
+    ingredient = Factory.build(:ingredient, :recipe => nil)
+    recipe.ingredients << ingredient
+    assert Recipe.find(recipe.id).ingredients.include?(Ingredient.find(ingredient.id))
+  end
 end
