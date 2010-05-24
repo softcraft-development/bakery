17c8445ba1885a244e650c97d49258ee7492359f
Craig Walker <github@softcraft.ca>
2010-05-24 14:30:02 -0600

    Removed unnecessary remove checkbox processing

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,11 +9,3 @@ function add_fields(link, association, content, handleContent) {
   var regexp = new RegExp("new_" + association, "g")
   $(link).before(content.replace(regexp, new_id));
 }
-
-jQuery(document).ready(function($){
-  $(".recipe_form .ingredients .ingredient .remove input[type='checkbox']").hide();
-  $(".recipe_form .ingredients .ingredient .remove label").click(function(){
-    $(this).parent().find("input[type='hidden']").val("1");
-    $(this).parent().parent().hide();
-  });
-});
