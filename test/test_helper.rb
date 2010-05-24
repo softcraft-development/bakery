544ef8ef8b9fb409a7c199102bb5949ee4e5999e
Craig Walker <github@softcraft.ca>
2010-05-23 19:57:32 -0600

    Added pp

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'factory_girl'
+require 'pp'
 # TODO: Reenable for Shoulda on Rails 3
 # require "shoulda"
 
