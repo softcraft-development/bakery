c9298da1bc774e59961ae041c37ee092a81701c9
Craig Walker <github@softcraft.ca>
2010-05-24 13:13:57 -0600

    Added removal processing

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,8 @@
 ;
-jQuery(document).ready(function(){
-	
+jQuery(document).ready(function($){
+  $(".recipe_form .ingredients .ingredient .remove input[type='checkbox']").hide();
+  $(".recipe_form .ingredients .ingredient .remove label").click(function(){
+    $(this).parent().find("input[type='hidden']").val("1");
+    $(this).parent().parent().hide();
+  });
 });
