a3a82986f18deb312e2a684dbd1a8990e6a07d70
Craig Walker <github@softcraft.ca>
2010-05-30 12:46:02 -0600

    Added loading from environment

--- config/initializers/load_app_config.rb
+++ config/initializers/load_app_config.rb
@@ -1,2 +1,3 @@
 raw_config = File.read("#{Rails.root}/config/app_config.yml")
 APP_CONFIG = YAML.load(raw_config)[Rails.env].symbolize_keys
+APP_CONFIG.merge! ENV # Heroku configuration comes from the environment
