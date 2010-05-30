d42e657e2e8c1e5b0885f5fd0e570a4f691abff2
Craig Walker <github@softcraft.ca>
2010-05-30 14:17:51 -0600

    Reworked config loading

--- config/initializers/load_app_config.rb
+++ config/initializers/load_app_config.rb
@@ -1,3 +1,7 @@
+require "hash_extensions"
+
 raw_config = File.read("#{Rails.root}/config/app_config.yml")
-APP_CONFIG = YAML.load(raw_config)[Rails.env].symbolize_keys
-APP_CONFIG.merge! ENV # Heroku configuration comes from the environment
+APP_CONFIG = ENV.to_hash.symbolize_keys # Heroku configuration comes from the environment
+all_config = YAML.load(raw_config).recursive_symbolize_keys!
+APP_CONFIG.merge! all_config[:common]
+APP_CONFIG.merge! all_config[Rails.env.to_sym]
