827e70a3a192b61766ef630ad135b666e0124b08
Craig Walker <github@softcraft.ca>
2010-05-30 18:50:18 -0600

    Turned off confirmable for development

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :lockable and :timeoutable
-  devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # , :lockable and :timeoutable
+  devise :database_authenticatable, 
+    :registerable, 
+    :token_authenticatable,
+    # :confirmable,
+    :recoverable, 
+    :rememberable, 
+    :trackable, 
+    :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation
