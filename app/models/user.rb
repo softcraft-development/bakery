5e2794bd40488e23df55758b7588dfb640068288
Craig Walker <github@softcraft.ca>
2010-05-30 21:32:31 -0600

    Added support for user roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,30 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation
+  attr_protected :role_list
+  before_save :set_role_list!
+  
+  def roles=(roles)
+    @role_set = Array(roles)
+  end
+
+  def roles
+    @role_set ||= load_roles!
+  end  
+  
+  def load_roles!(list = self.role_list)
+    if list.nil? || list.empty?
+      @role_set = []
+    else
+      @role_set = list.split(' ').map {|name| name.to_sym}
+    end
+  end
+  
+  def set_role_list!(set = @role_set || nil)
+    unless set.nil?
+      sorted = set.sort { |a,b| a.to_s.downcase <=> b.to_s.downcase }
+      of_symbols = sorted.select {|item| item.is_a? Symbol}
+      self.role_list = of_symbols.join(" ")         
+    end
+  end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -10,4 +10,42 @@ class UserTest < ActiveSupport::TestCase
   def test_user_email_required
     assert !Factory.build(:user, :email => nil).valid?
   end
+  
+  def test_role_retrieval
+    user = Factory.build(:user)
+    test_roles = :test1, :test2
+    user.load_roles!(test_roles.join(" "))
+    assert_equal 0, (user.roles - test_roles).size
+  end
+  
+  def test_role_set
+    user = Factory.build(:user)
+    test_roles = :test1, :test2
+    user.roles = test_roles
+    role_list = user.set_role_list!
+    assert_equal test_roles.join(" "), role_list
+  end
+  
+  def test_role_set_on_save
+    user = Factory.build(:user)
+    test_roles = :test1, :test2
+    user.roles = test_roles
+    user.save!
+    assert_equal test_roles.join(" "), user.send(:role_list)
+  end
+
+  def test_role_set_with_non_symbols
+    user = Factory.build(:user)
+    test_roles = :test1, :test2
+    user.roles = test_roles + ["test3", 4]
+    role_list = user.set_role_list!
+    assert_equal test_roles.join(" "), role_list
+  end
+  
+  def test_role_clear
+    user = Factory.build(:user)
+    user.roles.clear
+    role_list = user.set_role_list!
+    assert_equal "", role_list
+  end
 end
