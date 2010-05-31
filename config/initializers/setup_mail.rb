a874d274cf05634b3d47a7448d65eab2e754fb3c
Craig Walker <github@softcraft.ca>
2010-05-30 18:04:55 -0600

    added default domain

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,3 +2,5 @@ settings = APP_CONFIG[:smtp].dup
 settings[:authentication] = settings[:authentication].to_sym unless settings[:authentication].nil?
 
 ActionMailer::Base.smtp_settings = settings
+
+ActionMailer::Base.default_url_options[:host] = APP_CONFIG[:domain]
