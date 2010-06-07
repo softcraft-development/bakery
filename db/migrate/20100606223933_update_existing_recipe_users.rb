c6795310aebfa7f6bdbbbb635513e948732c8bb0
Craig Walker <github@softcraft.ca>
2010-06-06 19:04:06 -0600

    missing file from last commit

--- db/migrate/20100606223933_update_existing_recipe_users.rb
+++ db/migrate/20100606223933_update_existing_recipe_users.rb
@@ -1,18 +1,26 @@
 class UpdateExistingRecipeUsers < ActiveRecord::Migration
   def self.up
     Recipe.all.each { |recipe|
-      user ||= load_user
+      user ||= UpdateExistingRecipeUsers.load_user
       recipe.user = user unless recipe.user
+      recipe.save!
     }
   end
 
   def self.down
     Recipe.all.each { |recipe|
       recipe.user = nil
+      recipe.save!
     }
   end
   
-  def load_user
+  def self.load_user
     user = User.admins.first
+    unless user
+      user = User.create(
+        :email => "#{Time.now.to_f}.UpdateExistingRecipeUsers.migration@softcraft.ca",
+        :password => rand(1000000).to_s + "123456"  )
+    end
+    return user
   end
 end
