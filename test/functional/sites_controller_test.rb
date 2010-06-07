52fa1ab54b3bdfa958f75ce8ccec73bdd47921a0
Craig Walker <github@softcraft.ca>
2010-06-06 21:04:36 -0600

    Moved site permissions tests into site_controller_test

--- test/functional/sites_controller_test.rb
+++ test/functional/sites_controller_test.rb
@@ -21,4 +21,12 @@ class SitesControllerTest < ActionController::TestCase
     get :show
     assert_response :redirect
   end
+  
+  def test_admin_site
+    assert Ability.new(Factory.build(:admin)).can?(:manage, :site)
+  end
+  
+  def test_non_admin_site
+    assert Ability.new(Factory.build(:user)).cannot?(:manage, :site)
+  end  
 end
