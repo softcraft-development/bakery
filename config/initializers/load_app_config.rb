8e18e3cdc8e6a9a7db54df6e56334ed1260a66c6
Craig Walker <github@softcraft.ca>
2010-06-13 16:23:20 -0600

    prevented error on missing app_config (necessary for heroku)

--- config/initializers/load_app_config.rb
+++ config/initializers/load_app_config.rb
@@ -1,7 +1,12 @@
 require "hash_extensions"
 
-raw_config = File.read("#{Rails.root}/config/app_config.yml")
-APP_CONFIG = ENV.to_hash.symbolize_keys # Heroku configuration comes from the environment
+app_config_file_name = "#{Rails.root}/config/app_config.yml"
+if File.exists? app_config_file_name
+  raw_config = File.read(app_config_file_name)
+  APP_CONFIG = ENV.to_hash.symbolize_keys # Heroku configuration comes from the environment
+else
+  APP_CONFIG = {}
+end
 all_config = YAML.load(raw_config).recursive_symbolize_keys!
 APP_CONFIG.merge! all_config[:common]
 APP_CONFIG.merge! all_config[Rails.env.to_sym]
