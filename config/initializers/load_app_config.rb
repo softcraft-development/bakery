9fa2205da584b84be2f72abd7b1e04d099af83fa
Craig Walker <github@softcraft.ca>
2010-07-10 12:19:04 -0600

    Reworked APP_CONFIG and mail settings

--- config/initializers/load_app_config.rb
+++ config/initializers/load_app_config.rb
@@ -3,9 +3,12 @@ require "hash_extensions"
 APP_CONFIG = ENV.to_hash.symbolize_keys # Heroku configuration comes from the environment
 
 app_config_file_name = "#{Rails.root}/config/app_config.yml"
+APP_CONFIG_SETTINGS = {}
 if File.exists? app_config_file_name
   raw_config = File.read(app_config_file_name)
   all_config = YAML.load(raw_config).recursive_symbolize_keys!
-  APP_CONFIG.merge! all_config[:common]
-  APP_CONFIG.merge! all_config[Rails.env.to_sym]
+  APP_CONFIG_SETTINGS.merge! all_config[:common]
+  APP_CONFIG_SETTINGS.merge! all_config[Rails.env.to_sym]
 end
+
+APP_CONFIG.merge! APP_CONFIG_SETTINGS
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,14 @@
-if APP_CONFIG[:smtp]
-  settings = APP_CONFIG[:smtp].dup
-  settings[:authentication] = settings[:authentication].to_sym unless settings[:authentication].nil?
+settings = { 
+  :address => APP_CONFIG[:email_smtp_host],
+  :port => APP_CONFIG[:email_smtp_port],
+  :domain => APP_CONFIG[:email_smtp_domain],
+  :user_name => APP_CONFIG[:email_smtp_user_name],
+  :password => APP_CONFIG[:email_smtp_password],
+  :enable_starttls_auto => APP_CONFIG[:email_smtp_enable_starttls_auto]
+  }
+settings[:authentication] = APP_CONFIG[:email_smtp_authentication].to_sym unless settings[:email_smtp_authentication].nil?
 
-  ActionMailer::Base.smtp_settings = settings
+ActionMailer::Base.smtp_settings = settings
 
-  ActionMailer::Base.default_url_options[:host] = APP_CONFIG[:domain]
-  Mail.register_interceptor(DevelopmentMailInterceptor) unless Rails.env.production?
-else
-  Rails.logger.warn "Could not find SMTP settings in APP_CONFIG"
-end
+ActionMailer::Base.default_url_options[:host] = APP_CONFIG[:email_smtp_host]
+Mail.register_interceptor(DevelopmentMailInterceptor) unless Rails.env.production?
