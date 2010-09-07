0ed877e7f89102ae581cf52c37a5b1574922f93e
Craig Walker <github@softcraft.ca>
2010-09-07 13:24:45 -0600

    Added jQuery.clickToEdit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,3 +9,27 @@ function add_fields(link, association, content, handleContent) {
   var regexp = new RegExp("new_" + association, "g")
   $(link).before(content.replace(regexp, new_id));
 }
+
+jQuery.fn.clickToEdit = function(){
+  var input = $(this);
+  input.hide();
+  var display = $("<span />");
+  if ( input.attr("id") !== undefined ){
+    display.attr("id", input.attr("id") + "-display" );
+  }
+  var classes = "input-display";
+  if ( input.attr("class") !== undefined ){
+    classes += " " + input.attr("class");
+  }
+  display.addClass(classes)
+  display.attr("title", "Click to Edit");
+  display.text(input.val());
+  input.before(display);
+  input.change(function(){
+    display.text(input.val());
+  });
+  display.click(function(){
+    display.hide();
+    input.show();
+  });
+}
